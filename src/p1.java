7282c0385cd28aa860d03b029d441c55976917da
linzuolvke <zilongmao@foxmail.com>
2024-04-28 22:34:34 +0800

    init

---
+++ src/p1.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+/**
+ * @Description:
+ * @Created by
+ * @Date 2024/4/28 8:39 下午
+ * @Author: 邻座旅客
+ */
+public class p1 {
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (target - nums[i] == nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+                continue;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+//        int[] nums = {2,7,11,15};
+        int[] nums = {3,2,4};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+
+    }
+}
